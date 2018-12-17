8046af9cb658240b23b393d94b8e2ef88f6cbe44
wur3 <wu.richard.98@gmail.com>
2018-12-17 15:11:12 -0500

    Minial funcitonal product

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from selenium import webdriver
 import time
 
+##############################################################
+#       Part 1: Loggin in
+##############################################################
+
 # username and password are read from the first two lines of login.txt
 with open('./login.txt') as input_file:
     username = next(input_file).rstrip()
@@ -25,7 +29,7 @@ time.sleep(1)
 driver.find_element_by_id('i0116').send_keys(username)
 
 # Submit username/email/phone number
-driver.find_element_by_id("idSIButton9").click()
+driver.find_element_by_id('idSIButton9').click()
 
 # wait 1 second for everything to load
 time.sleep(1)
@@ -34,4 +38,30 @@ time.sleep(1)
 driver.find_element_by_id('i0118').send_keys(password)
 
 # Submit password
-driver.find_element_by_id("idSIButton9").click()
+driver.find_element_by_id('idSIButton9').click()
+
+##############################################################
+#       Part 2: Searching
+##############################################################
+
+# prepare list of filler search words (10 words)
+with open('words.txt') as input_file:
+    words = [next(input_file).rstrip() for x in range(10)]
+
+for i in range(10):
+    # wait 1 second for everything to load
+    time.sleep(1)
+
+    search_bar = driver.find_element_by_id('sb_form_q')
+
+    #Clear old content of search bar first
+    search_bar.clear()
+
+    #Inputs content into search bar
+    search_bar.send_keys(words[i])
+
+    # wait 1 second for everything to load
+    time.sleep(1)
+
+    #Submits input
+    driver.find_element_by_id('sb_form_go').click()
